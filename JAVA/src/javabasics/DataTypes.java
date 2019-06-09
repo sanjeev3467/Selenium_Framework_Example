18e0dbc9bf963f00d9819a892cf11fd9d1deadf9
sanjeev3467 <sanjeevnaidu638@gmail.com>
2019-06-09 11:07:56 +0530

    first commit

---
+++ JAVA/src/javabasics/DataTypes.java
@@ -0,0 +1,56 @@
+package javabasics;
+
+public class DataTypes {
+	
+	//This is my first java code
+	
+	/* hi this is my
+	 first java code
+	 for me*/
+	
+    //main()--main method-- Starting or execution point of the program
+	public static void main(String[] args) {
+		
+		//Primitive data types --- int, char, double, boolean only
+		
+		//data types
+			
+		//1.int;
+		
+		int i=10;
+		i=20;
+		int j=30;
+		int k=-1;
+		int m=0;
+		
+		//2.double --- float value like 10.22
+		
+		double n=21.33;
+		double p=63.58;
+		double q=100; //it will consedered as 100.00 float value
+		
+		//3. char --  it will considered as single digit 'a' or '1'
+		
+		char c='a';
+		char c1='b';
+		char c2='1';
+		char c3='$';
+		
+		//4.boolean--- true/fase
+		
+		boolean b1=true;
+		boolean b2=false;
+		
+		//String-- String is class, it is not a data type.
+		
+		String s="hello world";
+		String s1="selenium";
+		String s2="this is java string class";
+		String s4="12222";
+		String s5="12.45";
+		
+		System.out.println(n+p);
+
+	}
+
+}
---
+++ JAVA/src/javabasics/StringConcatination.java
@@ -0,0 +1,10 @@
+package javabasics;
+
+public class StringConcatination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
